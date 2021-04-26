fb487374f9d711e49dba3681749d89643e6b05e5
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-04-27 00:35:27 +0900

    Chapter1 done!

--- MyMemory/MyMemory/MemoListVC.swift
+++ MyMemory/MyMemory/MemoListVC.swift
@@ -36,6 +36,14 @@ class MemoListVC: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        let row = self.appDelegate.memolist[indexPath.row]
+        
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "MemoRead") as? MemoReadVC else {
+            return
+        }
+        
+        vc.param = row
+        self.navigationController?.pushViewController(vc, animated: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
