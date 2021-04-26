6e92ce9573af15f35493d96761441bcd74445003
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-04-26 23:56:53 +0900

    1.2.2 done!

---
+++ MyMemory/MyMemory/MemoData.swift
@@ -0,0 +1,16 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by justin dongwook Jung on 2021/04/26.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx : Int?
+    var title : String?
+    var contents : String?
+    var image : UIImage?
+    var regdate : Date?
+}
