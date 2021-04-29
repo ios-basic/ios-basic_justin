368f8fa828484610aed7a0f23ae0262a5dafeaf4
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-04-30 02:16:44 +0900

    Chapter03 wip

---
+++ Chapter03-TabBar/Chapter03-TabBar/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  Chapter03-TabBar
+//
+//  Created by justin dongwook Jung on 2021/04/30.
+//
+
+import UIKit
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
+        
+        title.text = "첫번째 탭"
+        title.textColor = .red
+        title.textAlignment = .center
+        title.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        title.sizeToFit()
+        
+        title.center.x = self.view.frame.width/2
+        
+        self.view.addSubview(title)
+        
+        self.tabBarItem.image = UIImage(named: "calendar.png")
+        self.tabBarItem.title = "Calendar"
+    }
+}
---
+++ Chapter03-TabBar/Chapter03-TabBar/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  Chapter03-TabBar
+//
+//  Created by justin dongwook Jung on 2021/04/30.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
+        
+        title.text = "세번째 탭"
+        title.textColor = .red
+        title.textAlignment = .center
+        title.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        title.sizeToFit()
+        
+        title.center.x = self.view.frame.width/2
+        
+        self.view.addSubview(title)
+        
+        self.tabBarItem.image = UIImage(named: "photo.png")
+        self.tabBarItem.title = "photo"
+    }
+}
