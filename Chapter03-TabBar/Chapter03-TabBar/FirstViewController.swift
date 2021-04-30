002e7018177f82a7b83a43f119330261c27607cc
jungdw0624 <jdw8998@daum.net>
2021-04-30 17:29:49 +0900

    3.1 done!

--- Chapter03-TabBar/Chapter03-TabBar/FirstViewController.swift
+++ Chapter03-TabBar/Chapter03-TabBar/FirstViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        overrideUserInterfaceStyle = .light
         let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
         
         title.text = "첫번째 탭"
@@ -23,5 +23,15 @@ class FirstViewController: UIViewController {
         
         self.view.addSubview(title)
         
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let tabBar = self.tabBarController?.tabBar
+//        tabBar?.isHidden = (tabBar?.isHidden == true) ? false : true
+        
+                
+        UIView.animate(withDuration: TimeInterval(0.15)){
+            tabBar?.alpha = ( tabBar?.alpha == 0 ? 1 : 0)
+        }
     }
 }
