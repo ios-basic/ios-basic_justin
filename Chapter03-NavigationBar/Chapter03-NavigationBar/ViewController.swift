4e10645b3b92d83b908abe4b80f9c9dcff7215bd
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-05-01 12:49:23 +0900

    3.2 done!

--- Chapter03-NavigationBar/Chapter03-NavigationBar/ViewController.swift
+++ Chapter03-NavigationBar/Chapter03-NavigationBar/ViewController.swift
@@ -12,50 +12,55 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        self.initTitle()
-        self.initTitleNew()
+        self.initTitleInput()
     }
     
-    func initTitleNew() {
-        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 36))
+    func initTitleInput(){
+        let tf = UITextField()
+        tf.frame = CGRect(x: 0, y: 0, width: 300, height: 35)
+        tf.backgroundColor = .white
+        tf.font = UIFont.systemFont(ofSize: 13)
+        tf.autocapitalizationType = .none
+        tf.autocorrectionType = .no
+        tf.spellCheckingType = .no
+        tf.keyboardType = .URL
+        tf.keyboardAppearance = .dark
+        tf.layer.borderWidth = 0.3
+        tf.layer.borderColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1.0).cgColor
         
-        let topTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 18))
-        topTitle.numberOfLines = 1
-        topTitle.textAlignment = .center
-        topTitle.font = UIFont.systemFont(ofSize: 15)
-        topTitle.textColor = .white
-        topTitle.text = "58개 숙소"
+        self.navigationItem.titleView = tf
         
-        let subTitle = UILabel(frame: CGRect(x: 0, y: 18, width: 200, height: 18))
-        subTitle.textAlignment = .center
-        subTitle.numberOfLines = 1
-        subTitle.font = UIFont.systemFont(ofSize: 12)
-        subTitle.textColor = .white
-        subTitle.text = "1박(1월 10일 ~ 1월 11일)"
+        let back = UIImage(named: "arrow-back")
+        let leftItem = UIBarButtonItem(image: back, style: .plain, target: self, action: nil)
         
-        containerView.addSubview(topTitle)
-        containerView.addSubview(subTitle)
+        self.navigationItem.leftBarButtonItem = leftItem
         
-        self.navigationItem.titleView = containerView
+        let rv = UIView()
+        rv.frame = CGRect(x: 0, y: 0, width: 70, height: 37)
         
-        let color = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
-        self.navigationController?.navigationBar.barTintColor = color
+        let rItem = UIBarButtonItem(customView: rv)
+        self.navigationItem.rightBarButtonItem = rItem
+        
+        let cnt = UILabel()
+        cnt.frame = CGRect(x: 10, y: 8, width: 20, height: 20)
+        cnt.font = UIFont.boldSystemFont(ofSize: 10)
+        cnt.textColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1.0)
+        cnt.text = "12"
+        cnt.textAlignment = .center
+        
+        cnt.layer.cornerRadius = 3
+        cnt.layer.borderWidth = 2
+        cnt.layer.borderColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1.0).cgColor
+        
+        rv.addSubview(cnt)
+        
+        let more = UIButton(type: .system)
+        more.frame = CGRect(x: 50, y: 10, width: 16, height: 16)
+        more.setImage(UIImage(named: "more"), for: .normal)
+        
+        rv.addSubview(more)
     }
     
-    func initTitle() {
-        let nTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
-        
-        nTitle.numberOfLines = 2
-        nTitle.textAlignment = .center
-        nTitle.textColor = .white
-        nTitle.font = UIFont.systemFont(ofSize: 15)
-        nTitle.text = "58개 숙소 \n 1박(1월 10일 ~ 1월 11일)"
-        
-        self.navigationItem.titleView = nTitle
-        
-        let color = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
-        self.navigationController?.navigationBar.barTintColor = color
-    }
 
 }
 
