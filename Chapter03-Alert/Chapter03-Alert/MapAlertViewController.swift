ebc524e3b4b4baab05b157cdb75a460e264667bf
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-05-02 21:22:00 +0900

    3.4 커스텀 클래스

--- Chapter03-Alert/Chapter03-Alert/MapAlertViewController.swift
+++ Chapter03-Alert/Chapter03-Alert/MapAlertViewController.swift
@@ -37,6 +37,14 @@ class MapAlertViewController: UIViewController {
         sliderBtn.addTarget(self, action: #selector(sliderAlert(_:)), for: .touchUpInside)
         
         self.view.addSubview(sliderBtn)
+        
+        let listBtn = UIButton(type: .system)
+        listBtn.frame = CGRect(x: 0, y: 300, width: 100, height: 30)
+        listBtn.center.x = self.view.frame.width/2
+        listBtn.setTitle("List Alert", for: .normal)
+        listBtn.addTarget(self, action: #selector(listAlert(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(listBtn)
     }
     
     @objc func mapAlert(_ sender: UIButton){
@@ -79,4 +87,22 @@ class MapAlertViewController: UIViewController {
         
         self.present(alert, animated: false)
     }
+    
+    @objc func listAlert(_ sender: Any){
+        let contentVC = ListViewController()
+        contentVC.delegate = self
+        
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        alert.setValue(contentVC, forKeyPath: "contentViewController")
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: false)
+    }
+    
+    func didSelectRowAt(indexPath: IndexPath){
+        print(">>> 선택된 행은 \(indexPath.row)입니다.")
+    }
+    
 }
