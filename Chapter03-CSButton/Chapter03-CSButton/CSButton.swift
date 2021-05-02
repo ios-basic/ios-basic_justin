5766d00cf13f8ef428b10fb0e967ec3f03c98b96
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-05-03 05:48:17 +0900

    3.5.3 스타일을 선택할 수 있는 버튼 만들기

--- Chapter03-CSButton/Chapter03-CSButton/CSButton.swift
+++ Chapter03-CSButton/Chapter03-CSButton/CSButton.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+public enum CSButtonType {
+    case rect
+    case circle
+}
+
 class CSButton: UIButton {
 
     required init(coder aDecoder: NSCoder) {
@@ -29,5 +34,54 @@ class CSButton: UIButton {
     
     init() {
         super.init(frame: CGRect.zero)
+    }
+    
+    convenience init(type: CSButtonType){
+        self.init()
+        
+        switch type {
+        case .rect:
+            self.backgroundColor = .black
+            self.layer.borderColor = UIColor.black.cgColor
+            self.layer.borderWidth = 2
+            self.layer.cornerRadius = 0
+            self.setTitleColor(.white, for: .normal)
+            self.setTitle("React Button", for: .normal)
+        case .circle:
+            self.backgroundColor = .red
+            self.layer.borderColor = UIColor.blue.cgColor
+            self.layer.borderWidth = 2
+            self.layer.cornerRadius = 50
+            self.setTitleColor(.white, for: .normal)
+            self.setTitle("Circle Button", for: .normal)
+        }
+        
+        self.addTarget(self, action: #selector(counting(_:)), for: .touchUpInside)
+    }
+    
+    var style: CSButtonType = .rect {
+        didSet {
+            switch style {
+            case .rect:
+                self.backgroundColor = .black
+                self.layer.borderColor = UIColor.black.cgColor
+                self.layer.borderWidth = 2
+                self.layer.cornerRadius = 0
+                self.setTitleColor(.white, for: .normal)
+                self.setTitle("React Button", for: .normal)
+            case .circle:
+                self.backgroundColor = .red
+                self.layer.borderColor = UIColor.blue.cgColor
+                self.layer.borderWidth = 2
+                self.layer.cornerRadius = 50
+                self.setTitleColor(.white, for: .normal)
+                self.setTitle("Circle Button", for: .normal)
+            }
+        }
+    }
+    
+    @objc func counting(_ sender: UIButton) {
+        sender.tag = sender.tag + 1
+        sender.setTitle("\(sender.tag) 번째 클릭", for: .normal)
     }
 }
