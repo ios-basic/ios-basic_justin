a97346849d9acab0fecaf3822d328af902b59259
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-05-02 22:43:42 +0900

    3.5.1 기본적인 커스텀 버튼 만들기

---
+++ Chapter03-CSButton/Chapter03-CSButton/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Chapter03-CSButton
+//
+//  Created by justin dongwook Jung on 2021/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let csBtn = CSButton()
+        csBtn.frame = CGRect(x: 30, y: 50, width: 150, height: 30)
+        self.view.addSubview(csBtn)
+    }
+
+
+}
+
