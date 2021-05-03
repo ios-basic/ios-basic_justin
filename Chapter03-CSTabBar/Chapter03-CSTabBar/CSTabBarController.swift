6d331bdbd29fc60858c1d1da1ee9c97efca01506
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-05-03 17:38:53 +0900

    3.6.2 탭 바 아이템인 척하는 	버튼 구현하기

--- Chapter03-CSTabBar/Chapter03-CSTabBar/CSTabBarController.swift
+++ Chapter03-CSTabBar/Chapter03-CSTabBar/CSTabBarController.swift
@@ -26,5 +26,39 @@ class CSTabBarController: UITabBarController {
         self.csView.backgroundColor = .brown
         
         self.view.addSubview(self.csView)
+        
+        let tabBtnWidth = self.csView.frame.size.width / 3
+        let tabBtnHeight = self.csView.frame.height
+        
+        self.tabItem01.frame = CGRect(x: 0, y: 0, width: tabBtnWidth, height: tabBtnHeight)
+        self.tabItem02.frame = CGRect(x: tabBtnWidth, y: 0, width: tabBtnWidth, height: tabBtnHeight)
+        self.tabItem03.frame = CGRect(x: tabBtnWidth*2, y: 0, width: tabBtnWidth, height: tabBtnHeight)
+        
+        self.addTabBarBtn(btn: self.tabItem01, title: "첫 번째 버튼", tag: 0)
+        self.addTabBarBtn(btn: self.tabItem02, title: "두 번째 버튼", tag: 1)
+        self.addTabBarBtn(btn: self.tabItem03, title: "세 번째 버튼", tag: 2)
+
+        self.onTabBarItemClick(self.tabItem01)
+    }
+    
+    func addTabBarBtn(btn: UIButton, title: String, tag: Int) {
+        btn.setTitle(title, for: .normal)
+        btn.tag = tag
+        
+        btn.setTitleColor(UIColor.white, for: .normal)
+        btn.setTitleColor(UIColor.yellow, for: .selected)
+        
+        btn.addTarget(self, action: #selector(onTabBarItemClick(_:)), for: .touchUpInside)
+        
+        self.csView.addSubview(btn)
+    }
+    
+    @objc func onTabBarItemClick(_ sender: UIButton) {
+        self.tabItem01.isSelected = false
+        self.tabItem02.isSelected = false
+        self.tabItem03.isSelected = false
+        
+        sender.isSelected = true
+        self.selectedIndex = sender.tag
     }
 }
