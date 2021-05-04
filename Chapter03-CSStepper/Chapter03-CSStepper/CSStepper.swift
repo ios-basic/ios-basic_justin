0f360182ec6dc93dbee779d0e3e23ff77a7d86a1
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-05-04 15:40:37 +0900

    3.7.4 커스터마이징을 위한 속성 추가하기

--- Chapter03-CSStepper/Chapter03-CSStepper/CSStepper.swift
+++ Chapter03-CSStepper/Chapter03-CSStepper/CSStepper.swift
@@ -14,12 +14,40 @@ class CSStepper: UIView {
     public var rightBtn = UIButton(type: .system)
     public var centerLabel = UILabel()
     
+    @IBInspectable
+    public var leftTitle: String = "⬇︎" {
+        didSet {
+            self.leftBtn.setTitle(leftTitle, for: .normal)
+        }
+    }
+    
+    @IBInspectable
+    public var rightTitle: String = "⬆︎" {
+        didSet {
+            self.rightBtn.setTitle(rightTitle, for: .normal)
+        }
+    }
+    
+    @IBInspectable
+    public var bgColor: UIColor = UIColor.cyan {
+        didSet {
+            self.centerLabel.backgroundColor = backgroundColor
+        }
+    }
+    
     //스테퍼의 현재값을 저장할 변수
+    @IBInspectable
     public var value: Int = 0 {
         didSet {
             self.centerLabel.text = String(value)
         }
     }
+    
+    @IBInspectable
+    public var stepValue: Int = 1
+    
+    @IBInspectable public var maximumValue: Int = 100
+    @IBInspectable public var minimumValue: Int = -100
     
     let borderWidth: CGFloat = 0.5
     let bordColor = UIColor.blue.cgColor
@@ -39,7 +67,7 @@ class CSStepper: UIView {
     
     private func setup(){
         self.leftBtn.tag = -1
-        self.leftBtn.setTitle("⬇︎", for: .normal)
+        self.leftBtn.setTitle(self.leftTitle, for: .normal)
         self.leftBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         
         self.leftBtn.layer.borderWidth = borderWidth
@@ -47,7 +75,7 @@ class CSStepper: UIView {
         
         self.rightBtn.tag = 1
         self.rightBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-        self.rightBtn.setTitle("⬆︎", for: .normal)
+        self.rightBtn.setTitle(self.rightTitle, for: .normal)
         
         self.rightBtn.layer.borderWidth = borderWidth
         self.rightBtn.layer.borderColor = bordColor
@@ -55,7 +83,7 @@ class CSStepper: UIView {
         self.centerLabel.text = String(value)
         self.centerLabel.font = UIFont.systemFont(ofSize: 16)
         self.centerLabel.textAlignment = .center
-        self.centerLabel.backgroundColor = .cyan
+        self.centerLabel.backgroundColor = self.bgColor
         self.centerLabel.layer.borderWidth = borderWidth
         self.centerLabel.layer.borderColor = bordColor
         
@@ -80,6 +108,13 @@ class CSStepper: UIView {
     }
     
     @objc public func valueChange(_ sender: UIButton){
-        self.value += sender.tag
+        
+        let sum = self.value + (sender.tag * self.stepValue)
+        
+        if (sum > self.maximumValue) || (sum < self.minimumValue) {
+            return
+        }
+        
+        self.value += sender.tag * self.stepValue
     }
 }
