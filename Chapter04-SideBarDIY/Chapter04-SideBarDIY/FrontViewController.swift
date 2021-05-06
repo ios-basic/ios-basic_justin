e21b667a56bebf5ae17b1e9ff2f09f8c22638b01
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-05-06 15:02:45 +0900

    wip

---
+++ Chapter04-SideBarDIY/Chapter04-SideBarDIY/FrontViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FrontViewController.swift
+//  Chapter04-SideBarDIY
+//
+//  Created by justin dongwook Jung on 2021/05/05.
+//
+
+import UIKit
+
+class FrontViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Chapter04-SideBarDIY/Chapter04-SideBarDIY/ReavealViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ReavealViewController.swift
+//  Chapter04-SideBarDIY
+//
+//  Created by justin dongwook Jung on 2021/05/05.
+//
+
+import UIKit
+
+class ReavealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
