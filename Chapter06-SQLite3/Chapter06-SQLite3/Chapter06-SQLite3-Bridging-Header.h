cb3781441df20d53221894c7d032199017c71bb3
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-05-11 15:07:18 +0900

    6.4.2 libsqlite3 실습 준비

---
+++ Chapter06-SQLite3/Chapter06-SQLite3/Chapter06-SQLite3-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Chapter06-SQLite3-Bridging-Header.h
+//  Chapter06-SQLite3
+//
+//  Created by justin dongwook Jung on 2021/05/11.
+//
+
+#import <sqlite3.h>
---
+++ Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter06-SQLite3
+//
+//  Created by justin dongwook Jung on 2021/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
