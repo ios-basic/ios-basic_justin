15643c79a2caa11f000b5db98c5ee3b273d4fadf
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-05-11 16:41:54 +0900

    Added controller

---
+++ Chapter06-HR/Chapter06-HR/DepartmentListVC.swift
@@ -0,0 +1,12 @@
+//
+//  DepartmentListVC.swift
+//  Chapter06-HR
+//
+//  Created by justin dongwook Jung on 2021/05/11.
+//
+
+import UIKit
+
+class DepartmentListVC: UITableViewController {
+
+}
---
+++ Chapter06-HR/Chapter06-HR/EmployeeListVC.swift
@@ -0,0 +1,12 @@
+//
+//  EmployeeListVC.swift
+//  Chapter06-HR
+//
+//  Created by justin dongwook Jung on 2021/05/11.
+//
+
+import UIKit
+
+class EmployeeListVC: UITableViewController {
+
+}
