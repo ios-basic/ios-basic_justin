29a53a732ddde6ada48ae618db57253928f34c86
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-05-11 15:31:01 +0900

    6.4.3 데이터베이스 연결과 테이블 생성

--- Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
+++ Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
@@ -10,8 +10,32 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // SQLite 연결 정보를 담는 객체
+        var db: OpaquePointer? = nil
+        // 컴파일된 SQL을 담는 객체
+        var stmt: OpaquePointer? = nil
+        
+        let fileMgr = FileManager()
+        let docPathURL = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let dbPath = docPathURL.appendingPathComponent("db.sqlite").path
+        
+        let sql = "CREATE TABLE IF NOT EXISTS sequence (num INTEGER)"
+        
+        if sqlite3_open(dbPath, &db) == SQLITE_OK {
+            if sqlite3_prepare(db, sql, -1, &stmt, nil) == SQLITE_OK {
+                if sqlite3_step(stmt) == SQLITE_DONE {
+                    print("Creae Table Success@")
+                }
+                sqlite3_finalize(stmt)
+            } else {
+                print("Prepare Statement Fail")
+            }
+            sqlite3_close(db)
+        } else {
+            print("Database Connect Fail")
+            return
+        }
+       
     }
 
 
