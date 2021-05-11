c5adb17d3f1ff5a0d00935d3cfb0db311ff2735c
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-05-11 15:52:06 +0900

    db.sqlite 파일 확인하기

--- Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
+++ Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
@@ -15,16 +15,17 @@ class ViewController: UIViewController {
         // 컴파일된 SQL을 담는 객체
         var stmt: OpaquePointer? = nil
         
-        let fileMgr = FileManager()
-        let docPathURL = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let dbPath = docPathURL.appendingPathComponent("db.sqlite").path
+//        let fileMgr = FileManager()
+//        let docPathURL = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first!
+//        let dbPath = docPathURL.appendingPathComponent("db.sqlite").path
+        let dbPath = "/Users/dongwookjung/db.sqlite"
         
         let sql = "CREATE TABLE IF NOT EXISTS sequence (num INTEGER)"
         
         if sqlite3_open(dbPath, &db) == SQLITE_OK {
             if sqlite3_prepare(db, sql, -1, &stmt, nil) == SQLITE_OK {
                 if sqlite3_step(stmt) == SQLITE_DONE {
-                    print("Creae Table Success@")
+                    print("Creae Table Success")
                 }
                 sqlite3_finalize(stmt)
             } else {
